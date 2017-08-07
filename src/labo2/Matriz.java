e144784a08dfac92dabb0180204b7bb2996b955e
Gabriel <Gabriel@Caravaca-PC>
2017-08-07 11:34:29 -0600

    full

--- src/labo2/Matriz.java
+++ src/labo2/Matriz.java
@@ -143,9 +143,9 @@ public class Matriz extends javax.swing.JFrame {
         for (int i = 0; i < fila ; i++) {
             if (matriz [i][0]==5){
                 cont+=1;
-                JOptionPane.showMessageDialog(null, "hay "+cont+" de numeros 5 en la primer columna de cada matriz");
             }
         }
+        JOptionPane.showMessageDialog(null, "hay "+cont+" de numeros 5 en la primer columna de cada matriz");
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
